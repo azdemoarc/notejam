a1924db829d8c7d7f9af75a6ffd654f5a240b380
azdemoarc <82955032+azdemoarc@users.noreply.github.com>
2021-04-25 03:19:40 +0200

    move db to az sql
    Move database from in-memory to azure sql database.

--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -1,13 +1,34 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+import urllib
+from sqlalchemy import create_engine
+
+
+driver = "{ODBC Driver 17 for SQL Server}"
+server = os.environ['server']
+database = os.environ['database']
+user = os.environ['user']
+password = os.environ['password']
+
+conn = f"""Driver={driver};Server=tcp:{server},1433;Database={database};
+Uid={user};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"""
+
+params = urllib.parse.quote_plus(conn)
+conn_str = 'mssql+pyodbc:///?autocommit=true&odbc_connect={}'.format(params)
+engine = create_engine(conn_str, echo=True)
+engine.execute("SELECT 1")
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = 'notejam-flask-secret-key'
+    SECRET_KEY = os.environ['secret_key']
     WTF_CSRF_ENABLED = True
-    CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
+    CSRF_SESSION_KEY = os.environ['csrf_key']
+    SQLALCHEMY_DATABASE_URI = conn_str
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
 
 
 class ProductionConfig(Config):
